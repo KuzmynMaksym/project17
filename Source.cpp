089569f108a251dc2c0419e72dcde3fc869825fc
KuzmynMaksym <144415346+KuzmynMaksym@users.noreply.github.com>
2023-11-03 12:05:13 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+void task_if1(); //Дано два числа. Вивести спочатку більше, а потім менше з них.
+int main()
+{
+	int a, b, max, min;
+	cin >> a >> b;
+	if (a > b)
+	{
+		max = a;
+		min = b;
+	}
+	else
+	{
+		max = b;
+		min = a;
+	}
+	cout << "max=" << max << "min=" << min;
+	return 0;
+}
